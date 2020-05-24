a619f0840596df795918f070df90dd821fe92f25
ManuPerezSan <beckem61@hotmail.com>
2020-05-24 22:40:44 +0200

    Upload Get-CtxClientVersion

---
+++ Get-CtxClientVersion.ps1
@@ -0,0 +1,81 @@
+﻿<#
+.SYNOPSIS
+    Connect to remote XenDesktop farm (CVAD) to obtain data about Client Receiver versions.
+
+.PARAMETER DdcServers
+    List of one Delivery Controller per farm.
+
+.PARAMETER Credential
+    Credentials to connect to remote server and XenDesktop farm (Read-Only)
+
+.RETURN
+    Return data in json format (data can be imported in influxDB)
+    
+.AUTHOR
+    Manuel Pérez
+
+.PROJECTURI
+    https://github.com/ManuPerezSan
+
+.REQUIREMENTS
+    Right to connect to Delivery Controller through WinRM
+    Read-only permissions in XenDesktop farm
+
+#>
+[CmdletBinding()]
+Param(
+[Parameter(Mandatory=$true, Position=0)][string[]]$DdcServers,
+[Parameter(Mandatory=$true, Position=1)][pscredential]$Credential
+)
+
+$clientVersions = @()
+
+Foreach($ddc in $DdcServers){
+
+    $s01 = New-PsSession -Computer $ddc -Credential $Credential
+
+    try{
+        
+        $returnedData = Invoke-command -Session $s01 -ScriptBlock{
+
+            $array=@()
+
+            Add-PSSnapin citrix.*
+            $sessions = Get-BrokerSession -AdminAddress localhost
+            
+            foreach ($i in ($sessions| group ClientVersion)){ 
+              
+              if ($i.Name -eq ""){
+                $version = '-'
+              }else{
+                $version = $i.Name
+              }
+
+              $Object = New-Object PSObject
+              $Statistics = New-Object PSObject
+
+              $Statistics | add-member Noteproperty UsedReceiverVersion_Qty $i.count
+
+              $Object | add-member Noteproperty ReceiverVersion $version
+              $Object | add-member Noteproperty Statistics $Statistics                
+
+              $array += $Object
+
+              
+            }
+            
+            return $array
+
+        } | Select -Property ReceiverVersion,Statistics
+
+    }catch{
+        Remove-PSSession $s01 -Confirm:$false
+    }
+
+    Remove-PSSession $s01 -Confirm:$false
+
+    $clientVersions += $returnedData
+
+}
+
+ConvertTo-Json $clientVersions -Depth 2 #-Compress
